f23fd5f92bd2590493e24c0fcb15ee4cb6242aa9
Binit Chandra Jha <binit.jha@outcodesoftware.com>
2026-06-27 17:23:26 +0545

    First push

---
+++ SwiftTodo/SwiftTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTodoApp.swift
+//  SwiftTodo
+//
+//  Created by Outcode on 27/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
